748ad9c5172552ff33d4135b3e5d313356abf201
Anton <Anton@192.168.9.232>
2025-03-10 11:56:11 -0600

    Prueba2

---
+++ src/Codigos_1/NewClass.java
@@ -0,0 +1,32 @@
+
+package codigos_1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class NewClass {
+    public static void main(String[] args) {
+        Queue<String> cola = new LinkedList<>();
+
+        agregarElemento(cola, "Hola");
+        agregarElemento(cola, "Como");
+        agregarElemento(cola, "estas");
+
+        buscarElemento(cola, "estas");
+        buscarElemento(cola, "bien");
+    }
+
+    public static void agregarElemento(Queue<String> cola, String elemento) {
+        cola.add(elemento);
+        System.out.println("Elemento \"" + elemento + "\" agregado a la cola.");
+    }
+
+    public static void buscarElemento(Queue<String> cola, String elemento) {
+        if (cola.contains(elemento)) {
+            System.out.println("Elemento \"" + elemento + "\" encontrado en la cola.");
+        } else {
+            System.out.println("Elemento \"" + elemento + "\" no se encuentra en la cola.");
+        }
+    }
+}
